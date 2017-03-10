4e8647a42b0d828ac3c27248121c8b214bb213fb
Arnaud Sahuguet <sahuguet@users.noreply.github.com>
2017-03-10 18:08:10 -0500

    Update nyuad_2017_directory.py

--- nyuad_2017_directory.py
+++ nyuad_2017_directory.py
@@ -10,7 +10,7 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 USERS = []
-with open('/Users/sahuguet/Downloads/NYU AD 2017 (Responses) - Form Responses 1.csv') as csvfile:
+with open('... CHANGE THE FOLDER NAME HERE .../NYU AD 2017 (Responses) - Form Responses 1.csv') as csvfile:
     reader = csv.reader(csvfile)
     reader.next()
     for row in reader:
