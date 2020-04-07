d892ade7fb5d34db4299c02acbec7582b575fe04
Fabricio Ph <fabriph@gmail.com>
2020-04-07 12:55:13 -0400

    Converts tuple to namedtuple

--- generate_directory.py
+++ generate_directory.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+from collections import namedtuple
 import csv
 import random
 import sys
@@ -27,19 +28,68 @@ def load_blacklist():
 		print >> sys.stderr, "INFO: blacklist.csv file not found."
 	return blacklist
 
-
+"""
+Loads a CSV file that contains <URL_FROM>,<URL_TO> for people who are having troubles getting the URL right, and the logic in this script deson't solve it
+"""
 def load_swaps():
 	swaps = {}
 	try:
 		with open(SWAPS_FILE) as swaps_fp:
 			reader = csv.reader(swaps_fp)
-			for row in reader:
-				(src, dst) = row
+			for rawRow in reader:
+				(src, dst) = rawRow
 				dst = dst.rstrip("\n\r")
 				swaps[src] = dst
 	except IOError as e:
 		print >> sys.stderr, "INFO: swaps file not found."
 	return swaps
+
+
+DataRow = namedtuple(
+	'DataRow',
+	[
+		'ttimestamp',
+		'email',
+		'name',
+		'facebook',
+		'linkedin',
+		'twitter',
+		'role',
+		'student_type',
+		'first_time',
+		'experience',
+		'affiliation',
+		'job_title',
+		'gender',
+		'country_ori',
+		'country_resi',
+		'bio_short',
+		'bio_long',
+		'picture',
+		'secret_power',
+		'secret_fact',
+		'offline_hobby',
+		'github',
+		'itunes',
+		'google_play',
+		'preferred_pl',
+		'preferred_frontend',
+		'preferred_backend',
+		'looking_for',
+		'tech_interest',
+		'topics',
+		'my_idea',
+		'ideation_talk_yes_no',
+		'technical_talk_yes_no',
+		'anything_else'])
+
+
+def convertDataRow(rawRow):
+	return DataRow(*rawRow)
+
+
+def shuffle_participants(data):
+	random.shuffle(data)
 
 
 def main():
@@ -58,120 +108,52 @@ def main():
 			reader = csv.reader(csvfile)
 			reader.next()
 			data = []
-			for row in reader:
-					(
-							ttimestamp,
-							email,
-							name,
-							facebook,
-							linkedin,
-							twitter,
-							role,
-							student_type,
-							first_time,
-							experience,
-							affiliation,
-							job_title,
-							gender,
-							country_ori,
-							country_resi,
-							bio_short,
-							bio_long,
-							picture,
-							secret_power,
-							secret_fact,
-							offline_hobby,
-							github,
-							itunes,
-							google_play,
-							preferred_pl,
-							preferred_frontend,
-							preferred_backend,
-							looking_for,
-							tech_interest,
-							topics,
-							my_idea,
-							ideation_talk_yes_no,
-							technical_talk_yes_no,
-							anything_else) = row
-					if (not picture):
-						data.append(row)
-					elif (email in blacklist):
-						data.append(row)
-					else:
-						picture_ok += 1
-						data.insert(0, row)
+			for rawRow in reader:
+				row = convertDataRow(rawRow)
+				if (not row.picture):
+					data.append(row)
+				elif (row.email in blacklist):
+					data.append(row)
+				else:
+					picture_ok += 1
+					data.insert(0, row)
 
 			# shuffle people that have valid pictures. Otherwise the default order is least recently edited in Google Forms.
 			data_copy = data[:picture_ok]
-			random.shuffle(data_copy)
+			shuffle_participants(data_copy)
 			data[:picture_ok] = data_copy
 
 			for row in data:
-					(
-								timestamp,
-								email,
-								name,
-								facebook,
-								linkedin,
-								twitter,
-								role,
-								student_type,
-								first_time,
-								experience,
-								affiliation,
-								job_title,
-								gender,
-								country_ori,
-								country_resi,
-								bio_short,
-								bio_long,
-								picture,
-								secret_power,
-								secret_fact,
-								offline_hobby,
-								github,
-								itunes,
-								google_play,
-								preferred_pl,
-								preferred_frontend,
-								preferred_backend,
-								looking_for,
-								tech_interest,
-								topics,
-								my_idea,
-								ideation_talk_yes_no,
-								technical_talk_yes_no,
-								anything_else) = row
 					# We process Google Dirve images.
-					if (picture in swaps):
-						picture = swaps[picture]
+					profile_picture_url = row.picture
+					if (profile_picture_url in swaps):
+						profile_picture_url = swaps[profile_picture_url]
 
-					picture = picture.replace(' ', '')
-					if picture.startswith('https://drive.google.com'):
-							slices = picture.split('/')
+					profile_picture_url = profile_picture_url.replace(' ', '')
+					if profile_picture_url.startswith('https://drive.google.com'):
+							slices = profile_picture_url.split('/')
 							if len(slices) == 7:
-									picture = 'https://drive.google.com/uc?export=view&id=' + slices[5]
+									profile_picture_url = 'https://drive.google.com/uc?export=view&id=' + slices[5]
 							elif len(slices) == 4:
-									picture = 'https://drive.google.com/uc?export=view&id=' + picture.split('=')[1]
+									profile_picture_url = 'https://drive.google.com/uc?export=view&id=' + profile_picture_url.split('=')[1]
 							else:
-									print >> sys.stderr, "ERROR %s (%s) with image %s" % (name, email, picture)
+									print >> sys.stderr, "ERROR %s (%s) with image %s" % (row.name, row.email, row.picture)
 
 					user = {
-							'name': name,
-							'role': role,
-							'affiliation': affiliation,
-							'picture': picture,
-							'country_ori': country_ori,
-							'country_resi': country_resi,
-							'bio': bio_short,
-							'secret_fact': secret_fact,
-							'secret_power': secret_power,
-							'looking_for': looking_for,
-							'tech_interest': tech_interest,
-							'offline_hobby': offline_hobby,
-							'linkedin': linkedin,
-							'github': github};
+							'name': row.name,
+							'role': row.role,
+							'affiliation': row.affiliation,
+							'picture': profile_picture_url,
+							'country_ori': row.country_ori,
+							'country_resi': row.country_resi,
+							'bio': row.bio_short,
+							'secret_fact': row.secret_fact,
+							'secret_power': row.secret_power,
+							'looking_for': row.looking_for,
+							'tech_interest': row.tech_interest,
+							'offline_hobby': row.offline_hobby,
+							'linkedin': row.linkedin,
+							'github': row.github};
 					users.append(user)
 
 
